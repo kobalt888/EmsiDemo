968c319c99d3306d70a8d29d3947cf3a32ae3138
Alfred <kobalt888@hotmail.com>
2019-02-04 19:28:04 -0800

    Created component scaffolding

---
+++ src/components/industries.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'industries',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/trends.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'trends',
+};
+</script>
+
+<style scoped>
+
+</style>
